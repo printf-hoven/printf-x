44ab7e02ca9ec85cf6fec4069f587617df94f5c5
printf-hoven <61111678+printf-hoven@users.noreply.github.com>
2025-06-20 18:47:12 +0530

    Add files via upload

---
+++ MCP_Server_Client/MCPClient/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using ModelContextProtocol.Client;
+
+var clientTransport = new SseClientTransport(new SseClientTransportOptions()
+{
+  Endpoint = new Uri("https://localhost:5001/"),
+  Name = "My_Client"
+});
+
+IMcpClient? mcpClient_t = null;
+
+try
+{
+  mcpClient_t = await McpClientFactory.CreateAsync(clientTransport);
+}
+catch (Exception ex)
+{
+  Console.WriteLine($"ERROR McpClientFactory.CreateAsync {ex.Message}");
+
+  Console.WriteLine("HAVE YOU STARTED THE SERVER?");
+
+  return;
+}
+
+await using IMcpClient mcpClient = mcpClient_t!;
+
+IList<McpClientTool>? tools = null;
+
+try
+{
+  tools = await mcpClient.ListToolsAsync();
+}
+catch (Exception ex)
+{
+  Console.WriteLine($"Error ListToolsAsync {ex.Message}");
+
+  return;
+}
+
+if (!tools.Any())
+{
+  Console.WriteLine("No tools found on the server. Quitting...");
+
+  return;
+}
+
+Console.WriteLine("Connected to server with tools:");
+
+foreach (var tool in tools)
+{
+  Console.WriteLine($"{tool.Name}, {tool.Description}");
+}
+
+Console.WriteLine($"Calling the first tool...");
+
+var result = await tools[0].CallAsync(new Dictionary<string, object?>() { ["n"] = 9 });
+
+Console.Write("Response is: ");
+Console.WriteLine(result.Content.First(c => c.Type == "text").Text);
+
+// for more ->
+// https://github.com/modelcontextprotocol/csharp-sdk/tree/main/samples
+
+
+
---
+++ MCP_Server_Client/MCPServer/Program.cs
@@ -0,0 +1,36 @@
+﻿// Install-Package ModelContextProtocol -prerelease
+// Install-Package ModelContextProtocol.AspNetCore -prerelease
+
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+  .AddMcpServer()
+  .WithHttpTransport()
+  .WithToolsFromAssembly();
+
+var app = builder.Build();
+
+app.MapMcp();
+
+app.Run("https://localhost:5001/");
+
+// ---- 
+
+[McpServerToolType]
+public sealed class EchoTool
+{
+  [McpServerTool, Description("Gives the square of a number")]
+  public static int MakeSquare(int n)
+  {
+    return n * n;
+  }
+
+  [McpServerTool, Description("Gives the cube of a number")]
+  public static int MakeCube(int n)
+  {
+    return n * n * n;
+  }
+}
